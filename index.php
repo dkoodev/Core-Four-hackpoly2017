1777c57071c33cd85cf9d510b481f4743ca4be30
Nishir <nshelat@usc.edu>
2017-02-11 12:17:30 -0800

    Created basic files

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Succinct</title>
+    <link rel="stylesheet" type="text/css" href="main.css">
+    <script src="script.js"></script>
+  </head>
+  <body>
+
+    <?php
+
+    ?>
+
+</body>
+</html>
